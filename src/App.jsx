f54b5a752b68fa6d42a85d7b9516c303ecbc6eaa
marcexl <mxlgallardo@gmail.com>
2023-02-25 15:00:58 -0300

    refetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import useGoogleSheets from 'use-google-sheets';
 import { Card } from 'primereact/card';
 import Video from './back.mp4';
 import './App.css'
+import { useEffect } from "react";
 function App (){
 
-  const { data, loading, error } =  useGoogleSheets({
+  const { data, loading, error, refetch} =  useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
   });
 
+  /*useEffect( () =>{
+    setInterval(() => {
+      refetch
+    }, 10000)
+  },[])*/
 
   if (loading) {
     return(<div className="loader"><ProgressSpinner /></div>)
@@ -21,7 +27,9 @@ function App (){
   if (error) {
     return <div>Error!</div>;
   }
-  let increment = 0.2;
+  let increment    = 0.2;
+  let newincrement = 0;
+
   if(data){
     const suenios = data[0]['data']; 
     return(
@@ -34,21 +42,22 @@ function App (){
         </div>
         <div className="banner">
           <h6>MIRAI HOMBU</h6>
-          <h2>LXS CHICXS HACEMOS HISTORIA</h2>
+          <h2>Lxs chicxs HACEMOS HISTORIA</h2>
         </div>
         <div className="suenios-container">
           {
             suenios.map((mirai, index) => {
-                increment = increment+increment
-                let idClass = Math.floor(Math.random() * 7)
-                return (
-                    <Card title={mirai.suenio} className={ `card-${idClass}`} style={{animationDelay: `${increment}`}} key={index}>
-                      <p className='m-0'>
-                        {mirai.nombre}
-                      </p>
-                    </Card>
-                );
-              })
+              newincrement = increment+newincrement
+              console.log(newincrement)
+              let idClass = Math.floor(Math.random() * 7)
+              return (
+                  <Card title={mirai.suenio} className={ `card-${idClass}`} style={{animationDelay: `${increment}`}} key={index}>
+                    <p className='m-0'>
+                      {mirai.nombre}
+                    </p>
+                  </Card>
+              );
+            })
           }
         </div>
       </div>
